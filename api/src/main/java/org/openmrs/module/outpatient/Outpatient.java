cf20b1b98418b4f9231aa5aed0b3763e57d0f93b
mashinsky <josephmaramba50@gmail.com>
2016-02-16 11:21:08 +0300

    Test commit.

--- api/src/main/java/org/openmrs/module/outpatient/Outpatient.java
+++ api/src/main/java/org/openmrs/module/outpatient/Outpatient.java
@@ -25,6 +25,7 @@ public class Outpatient extends BaseOpenmrsObject implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	private String na;
 	
 	@Override
 	public Integer getId() {
